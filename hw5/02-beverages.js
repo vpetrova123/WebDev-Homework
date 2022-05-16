f89d14f4b1f58c55f0e5c9f75bba080ab1f05232
Viktoriya <vpetrova@pdx.edu>
2022-05-16 14:40:09 -0700

    Completed HW5

--- hw5/02-beverages.js
+++ hw5/02-beverages.js
@@ -1,10 +1,54 @@
 const url =
-  'https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic';
+  "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic";
 
-let app = document.querySelector('#results');
+let app = document.querySelector("#results");
+
+const addToDOM = (item) => {
+  console.log(item);
+  let element = document.createElement("div");
+  let img = document.createElement("img");
+  let name = document.createElement("p");
+
+  element.className = "figure";
+  element.style.height = "60%";
+  element.style.width = "200px";
+  element.style.margin = "10px";
+
+  img.style.width = "100%";
+  img.style.height = "100%";
+
+  name.style.height = "60px";
+  name.style.marginTop = "10px";
+  name.style.marginBottom = "50px";
+  name.style.fontSize = "24px";
+
+  img.src = item.strDrinkThumb;
+  name.textContent = item.strDrink;
+
+  element.append(img);
+  element.append(name);
+  app.append(element);
+};
 
 const fetchData = (url) => {
-  // Add your code here
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.drinks);
+      data.drinks.forEach((item) => {
+        addToDOM(item);
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+      let element = document.createElement("div");
+      element.textContent = "An error occured. Please try again.";
+      app.append(element);
+    })
+    .finally(() => {
+      let loader = document.querySelector("#loading");
+      app.removeChild(loader);
+    });
 };
 
 fetchData(url);
--- hw5/03-beverages.js
+++ hw5/03-beverages.js
@@ -1,6 +1,55 @@
 $(document).ready(function () {
   const url =
-    'https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic';
+    "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic";
 
-  // Add your code here
+  const addToDOM = (item) => {
+    var img = new Image();
+    img.src = item.strDrinkThumb;
+    $("#results").append(
+      $("<div class='figure'>")
+        .css({
+          height: "60%",
+          width: "200px",
+          margin: "10px",
+        })
+        .append(
+          $(img).css({
+            width: "100%",
+            height: "100%",
+          })
+        )
+        .append(
+          $("<p>").text(item.strDrink).css({
+            height: "60px",
+            marginTop: "10px",
+            marginBottom: "50px",
+            fontSize: "24px",
+          })
+        )
+    );
+  };
+
+  const fetchData = (url) => {
+    $.ajax({
+      type: "GET",
+      url: url,
+      success: (data) => {
+        console.log(data.drinks);
+        data.drinks.forEach((item) => {
+          addToDOM(item);
+        });
+      },
+      error: (error) => {
+        console.log(error);
+        $("#results").append(
+          $("<div>").text(`An error occured. Please try again.`)
+        );
+      },
+      complete: () => {
+        $("#loading").remove();
+      },
+    });
+  };
+
+  fetchData(url);
 });
